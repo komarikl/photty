1a04d9c7b5b5a2078a6b03fb33d8c377183b3ceb
Lada Ilyina <lada_ilyina@epam.com>
2017-07-17 01:38:40 +0300

    first draft of the photty

---
+++ gulp/tasks/browserify.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('browserify', function() {
+    return $.browserify($.path.app)
+		.bundle()
+		.pipe(source('app.min.js'))
+		.pipe($.gulp.dest($.config.root + '/assets/js'))
+	}
+  )
+};
+ 
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js'),
+    jsFoundation: require('./gulp/paths/js.foundation.js'),
+    cssFoundation: require('./gulp/paths/css.foundation.js'),
+    app: require('./gulp/paths/app.js')
+  },
+  gulp: require('gulp'),
+  del: require('del'),
+  merge: require('merge-stream'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')({
+      rename: {
+          'gulp-css-unit': 'cssunit'
+      }
+  }),
+  source: require('vinyl-source-stream'),
+  browserify: require('browserify'),
+  watchify: require('watchify'),
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'sass',
+    'pug',
+    'js:foundation',
+    'js:process',
+    'copy:image',
+    'copy:fonts',    
+    'css:foundation',
+    'sprite:svg',
+    'sprite:png'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
